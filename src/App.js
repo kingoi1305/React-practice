7f81357161362b0d096762e5ff9683b56e9b8970
Kingo <kingoi1305@gmail.com>
2021-12-22 00:16:23 +0900

    commit2021 12 22

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Header from './components/Header/Header'
+import YSearch from 'youtube-api-search';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+const YOUTUBE_API_KEY = 'AIzaSyDBUeh6T6T4vsOJiJd3WH4_gw8agXEZS3c'
+
+class App extends Component {
+
+  state = { videos: [] }
+
+
+  componentDidMount(){
+    YSearch({ key: YOUTUBE_API_KEY, term: 'soccer サッカー' }, (data) => {
+      this.setState({videos: data});
+    });
+  }
+
+  render() {
+    return(
+
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+
+  //state初期化
+  state = {keyword: ''}
+
+  //value取得 event handler
+  onInputChangeHandler = (event) => {
+    this.setState({keyword: event.target.value})
+  }
+
+  render(){
+    return(
+      <div style={{textAlign: 'center'}}>
+        <div>
+          <h2>FootballTube</h2>
+        </div>
+        <div>
+          <input onChange={this.onInputChangeHandler} value={this.state.keyword} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Header;
