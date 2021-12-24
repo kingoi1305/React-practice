7fe956647bc1d9a578cbf16ed9848c5a5819a441
Kingo <kingoi1305@gmail.com>
2021-12-25 00:35:01 +0900

    commit

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-
 class Header extends Component {
 
   //state初期化
@@ -7,19 +6,17 @@ class Header extends Component {
 
   //value取得 event handler
   onInputChangeHandler = (event) => {
-    // this.setState({keyword: event.target.value})
+    this.setState({keyword: event.target.value});
   }
 
   render(){
     return(
-      <div style={{textAlign: 'center'}}>
-        <div>
-          <h2>FootballTube</h2>
-        </div>
-        <div>
-          <input onChange={this.onInputChangeHandler} value={this.state.keyword} />
-        </div>
-      </div>
+      <nav className='navbar navbar-expand navbar-light bg-light mb-3 p-3'>
+          <h2 className='mb-0 me-2'>footballtube</h2>
+        <form className='form-group my-2 my-log-0 ml-sm-2'>
+          <input onChange={this.onInputChangeHandler} value={this.state.keyword} className='form-control form-control-lg mr-sm-2' type='text' placeholder='Search...' aria-label='Search...' />
+        </form>
+      </nav>
     );
   }
 }
