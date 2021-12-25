a72ae2279ca1b4bcef1093c189cc21d6e0c49b1d
Kingo <kingoi1305@gmail.com>
2021-12-25 13:16:36 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -6,28 +6,36 @@ import Body from './components/Body/Body';
 import List from './components/Video/List/List';
 import Video from './components/Video/Video';
 
-const YOUTUBE_API_KEY = 'AIzaSyDBUeh6T6T4vsOJiJd3WH4_gw8agXEZS3c'
+const YOUTUBE_API_KEY = 'AIzaSyAOIsfVLvIyzcYHhgLWe2NPSrwjS_99L1s'
 
 class App extends Component {
 
-  state = { videos: [] }
+  state = { videos: [],
+            selectedVideo: null}
 
 
   componentDidMount(){
     YSearch({ key: YOUTUBE_API_KEY, term: 'soccer'}, (data) => {
-      this.setState({videos: data})
+      this.setState({videos: data, selectedVideo: data[2]})
     });
+  }
+
+  onVideoClickedHandler = (video) => {
+    this.setState({ selectedVideo: video })
   }
 
   render() {
     return(
-
       <div className="App">
         <Header />
 
         <Body>
-          <Video video={this.state.videos[0]} />
-          <List videos={this.state.videos} />
+          <Video video={this.state.selectedVideo} />
+          <List 
+            videos={this.state.videos}
+            onVideoClicked={this.onVideoClickedHandler}
+            selectedVideo={this.state.selectedVideo}
+          />
         </Body>
       </div>
     );
